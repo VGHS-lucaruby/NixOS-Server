42c456fce411deb213ca539d3b8e5772b0634cbd
Luca <lucacrisafi@gmail.com>
2024-07-09 22:57:18 +0100

    Add missing qemu stuff

--- Hosts/DATHOMINECRAFT01/hardware-configuration.nix
+++ Hosts/DATHOMINECRAFT01/hardware-configuration.nix
@@ -1,8 +1,6 @@
-{ config, lib, pkgs, modulesPath, ... }:
+{ config, lib, pkgs, ... }:
 
 {
-  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
-
   boot.initrd.availableKernelModules = [ "uhci_hcd" "ehci_pci" "ahci" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
--- NixOS/Base/QEMU.nix
+++ NixOS/Base/QEMU.nix
@@ -1,9 +1,11 @@
-{ ... }:
+{ modulesPath, ... }:
 
 {
-  imports = [
-    "${nixpkgs}/nixos/modules/virtualisation/qemu-vm.nix";
+  imports = [ 
+    (modulesPath + "/profiles/qemu-guest.nix")
+    (modulesPath + "/virtualisation/qemu-vm.nix")
   ];
+
   
   virtualisation.qemu.guestAgent.enable = true;
 
