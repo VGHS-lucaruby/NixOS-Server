7d96f611c88954af833b49f57759b7f3aa9360c5
Luca <lucacrisafi@gmail.com>
2024-07-17 21:15:26 +0100

    Define Reboot Window

--- NixOS/Base/AutoUpgrade.nix
+++ NixOS/Base/AutoUpgrade.nix
@@ -3,10 +3,14 @@
 {
   system.autoUpgrade = {
     enable = true;
+    flake = "github:VGHS-lucaruby/NixOS-Server#${nodeHostName}";  
     dates = "Sat *-*-* 03:00 Europe/London";
     # randomizedDelaySec = "2hr";
     allowReboot = true;
-    flake = "github:VGHS-lucaruby/NixOS-Server#${nodeHostName}";  
+    rebootWindow = {
+      lower = "03:00";
+      upper = "05:00";
+    };
     flags = [
       "-L" # print build logs
       "--refresh" # update the repository
