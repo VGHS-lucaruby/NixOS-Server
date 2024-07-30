a7c869256d27f4c68d62608749b8ecd17a560c43
Luca <lucacrisafi@gmail.com>
2024-07-30 23:29:30 +0100

    Test

--- NixOS/Base/Sops.nix
+++ NixOS/Base/Sops.nix
@@ -4,9 +4,14 @@
   sops = {  
     defaultSopsFile = "${nodeSecrets}/${nodeHostName}.yaml";
     validateSopsFiles = true;
+    gnupg.sshKeyPaths = [];
     age = {
       # This will automatically import SSH keys as age keys
       sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+      # This is using an age key that is expected to already be in the filesystem
+      keyFile = "/var/lib/sops-nix/key.txt";
+      # This will generate a new key if the key specified above does not exist
+      generateKey = true;
     };
   };
 }
