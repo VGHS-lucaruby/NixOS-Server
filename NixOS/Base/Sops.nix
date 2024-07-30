e4514eb235c8e6767b050a675e3e8e7d3224ab1e
Luca <lucacrisafi@gmail.com>
2024-07-30 22:40:56 +0100

    Test

--- NixOS/Base/Sops.nix
+++ NixOS/Base/Sops.nix
@@ -2,19 +2,11 @@
 
 {
   sops = {  
-    # This will add secrets.yml to the nix store
-    # You can avoid this by adding a string to the full path instead, i.e.
-    # sops.defaultSopsFile = "/root/.sops/secrets/example.yaml";
     defaultSopsFile = "${nodeSecrets}/${nodeHostName}.yaml";
-    
     validateSopsFiles = true;
     age = {
       # This will automatically import SSH keys as age keys
       sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-      # This is using an age key that is expected to already be in the filesystem
-      keyFile = "/var/lib/sops-nix/key.txt";
-      # This will generate a new key if the key specified above does not exist
-      generateKey = true;
     };
   };
 }
