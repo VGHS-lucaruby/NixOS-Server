ba68953305680b6f747283103dfa069b34d65617
Luca <lucacrisafi@gmail.com>
2024-08-06 01:33:29 +0100

    Debug World Creation

--- NixOS/Minecraft/default.nix
+++ NixOS/Minecraft/default.nix
@@ -4,7 +4,7 @@
   imports = [
     inputs.nix-minecraft.nixosModules.minecraft-servers
     ./Nix-Minecraft.nix
-    ./FabricTest.nix
+    ./WorldCreation2.0.nix
   ];
   nixpkgs.overlays = [inputs.nix-minecraft.overlay];
 }
