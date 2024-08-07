26f19207e3c5be4461faffba93e827d4cb5e388f
Luca <lucacrisafi@gmail.com>
2024-08-07 18:05:09 +0100

    WIP Postgres

---
+++ NixOS/Postgresql/Postgres.nix
@@ -0,0 +1,31 @@
+{ pkgs, ... }:
+
+{
+  services.postgresql = {
+    enable = true;
+    ensureDatabases = [ "Authentik" ];
+    enableTCPIP = true;
+    port = 5432;
+    package = pkgs.postgresql_15;
+    authentication = pkgs.lib.mkOverride 10 ''
+      # Generated file do not edit
+      # Type   database   DBuser   origin-address   auth-method
+      local    all        all                       trust
+      host     all        all      10.0.0.0/16      scram-sha-256
+    '';
+    ensureUsers = [
+      {
+        name = "Authentik";
+        # Todo Rest of perms
+      }
+    ];
+
+    # Todo Backups
+
+    # initialScript = pkgs.writeText "backend-initScript" ''
+    #   CREATE ROLE nixcloud WITH LOGIN PASSWORD 'nixcloud' CREATEDB;
+    #   CREATE DATABASE nixcloud;
+    #   GRANT ALL PRIVILEGES ON DATABASE nixcloud TO nixcloud;
+    # '';
+  };
+}
