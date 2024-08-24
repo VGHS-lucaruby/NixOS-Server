a22e2e761707377e84083f60b79b4635255aab77
VGHS-lucaruby <lucacrisafi@gmail.com>
2024-08-24 15:46:36 +0100

    Update MC

--- NixOS/Minecraft/WorldCreation2.0.nix
+++ NixOS/Minecraft/WorldCreation2.0.nix
@@ -2,8 +2,8 @@
 
 let
   modpack = pkgs.fetchPackwizModpack {
-    url = "https://raw.githubusercontent.com/VGHS-lucaruby/Minecraft-WorldCreation/2.0.0-RC11/pack.toml";
-    packHash = "sha256-aA5hnWf2Skc+Q3I5854VHhjot7MDH2ngNc618DUPvjI=";
+    url = "https://raw.githubusercontent.com/VGHS-lucaruby/Minecraft-WorldCreation/2.0.0-RC12/pack.toml";
+    packHash = "sha256-wAPWPOny68u8/beuuzDQg+VyKMA1mqz/R2AYGaDVaN4=";
   };
 
   mcVersion = modpack.manifest.versions.minecraft;
