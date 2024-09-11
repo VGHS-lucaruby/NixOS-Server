e5619c78fca866116a9203391034ad45bf912d42
Endercute <lpietzk@gmx.de>
2024-09-11 20:30:38 +0200

    Initial Layout Arma server

---
+++ Nodes/DATHOARMA01.nix
@@ -0,0 +1,19 @@
+{ ... }:
+
+{
+  # Set IP
+  networking = {  
+    interfaces.eth0.ipv4.addresses = [ 
+      {
+        address = "10.0.20.102";
+        prefixLength = 24;
+      } 
+    ];
+  };
+
+  # Import Modules
+  # Example: imports = [ ../NixOS/Minecraft ];
+
+  # Configure Modules
+  # Example: modExample.enable = true;
+}
