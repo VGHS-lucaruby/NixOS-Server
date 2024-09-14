d3fe02f73db94cf45dfe4e37a125887f4167a585
Luca <lucacrisafi@gmail.com>
2024-09-14 13:43:40 +0100

    Typo

--- NixOS/Base/SteamDownloader.nix
+++ NixOS/Base/SteamDownloader.nix
@@ -45,7 +45,7 @@
 	  			"
 
 	  			${pkgs.steamcmd}/bin/steamcmd $cmds
-	  		''};
+	  		''}";
 	  		PrivateTmp = true;
 	  		Restart = "on-failure";
 	  		TimeoutStartSec = 3600; # Allow time for updates.
