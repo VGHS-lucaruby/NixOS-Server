87c808350edecdfc5775142c5a7685826c3bd4ed
Luca <lucacrisafi@gmail.com>
2024-09-22 03:46:28 +0100

    Add LDAP

--- NixOS/Authentik/Authentik.nix
+++ NixOS/Authentik/Authentik.nix
@@ -7,26 +7,32 @@
     "Authentik/envRADIUS" = {};
   };
 
-  services.authentik = {
-    enable = true;
-    environmentFile = config.sops.secrets."Authentik/env".path;
-    createDatabase = false;
-    settings = {
-      postgresql = {
-        user = "authentik";
-        name = "authentik";
-        host = "10.0.20.50";
+  services = {  
+    authentik = {
+      enable = true;
+      environmentFile = config.sops.secrets."Authentik/env".path;
+      createDatabase = false;
+      settings = {
+        postgresql = {
+          user = "authentik";
+          name = "authentik";
+          host = "10.0.20.50";
+        };
+        # email = {
+        #   host = "smtp.example.com";
+        #   port = 587;
+        #   username = "authentik@example.com";
+        #   use_tls = true;
+        #   use_ssl = false;
+        #   from = "authentik@example.com";
+        # };
+        disable_startup_analytics = true;
+        avatars = "initials";
       };
-      # email = {
-      #   host = "smtp.example.com";
-      #   port = 587;
-      #   username = "authentik@example.com";
-      #   use_tls = true;
-      #   use_ssl = false;
-      #   from = "authentik@example.com";
-      # };
-      disable_startup_analytics = true;
-      avatars = "initials";
+    };
+    authentik-ldap = { 
+      enable = true;
+      environmentFile = config.sops.secrets."Authentik/envLDAP".path;
     };
   };
 }
