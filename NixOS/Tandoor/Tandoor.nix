8b90c0aee22b053f87f5b192695ec7ed875a1262
Luca <lucacrisafi@gmail.com>
2024-10-06 21:26:53 +0100

    Update LDAP Args

--- NixOS/Tandoor/Tandoor.nix
+++ NixOS/Tandoor/Tandoor.nix
@@ -32,7 +32,8 @@
       POSTGRES_DB="tandoor";
       POSTGRES_PORT=5432;
       LDAP_AUTH=1;
-      AUTH_LDAP_START_TLS=1;
+      AUTH_LDAP_ALWAYS_UPDATE_USER=1;
+      AUTH_LDAP_CACHE_TIMEOUT=3600;
       AUTH_LDAP_SERVER_URI="ldaps://ldaps.${primaryDomain}:636";
       AUTH_LDAP_USER_SEARCH_FILTER_STR="(&(objectclass=user)(memberOf=cn=tandoor))";
       AUTH_LDAP_USER_ATTR_MAP="{'username': 'cn', 'email': 'mail'}";
