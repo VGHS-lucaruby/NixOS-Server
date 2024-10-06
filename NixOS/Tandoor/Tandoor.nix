116edc3cc14f9d7e083a259654535db160e6805a
Luca <lucacrisafi@gmail.com>
2024-10-06 18:41:18 +0100

    Testing Ensure DB Engine Is set

--- NixOS/Tandoor/Tandoor.nix
+++ NixOS/Tandoor/Tandoor.nix
@@ -27,13 +27,14 @@
     address = "0.0.0.0";
     extraConfig = {
       ALLOWED_HOSTS="recipes.${primaryDomain}";
+      DB_ENGINE="django.db.backends.postgresql";
       POSTGRES_HOST="10.0.20.50";
       POSTGRES_DB="tandoor";
       POSTGRES_PORT=5432;
       LDAP_AUTH=1;
       AUTH_LDAP_SERVER_URI="ldaps://ldaps.${primaryDomain}:636";
-      AUTH_LDAP_USER_SEARCH_FILTER_STR = "(&(objectclass=user)(memberOf=cn=tandoor)))";
-      AUTH_LDAP_USER_ATTR_MAP = "{'username': 'cn', 'email': 'mail'}";
+      AUTH_LDAP_USER_SEARCH_FILTER_STR="(&(objectclass=user)(memberOf=cn=tandoor)))";
+      AUTH_LDAP_USER_ATTR_MAP="{'username': 'cn', 'email': 'mail'}";
     };
   };
 }
