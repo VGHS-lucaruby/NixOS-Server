2b60b497b9d5a8c6576bbaeb657ddff94359d81e
Luca <lucacrisafi@gmail.com>
2024-10-09 07:29:59 +0100

    Fix Broken LDAP (Tandoor's Config Is Strange)

--- NixOS/Tandoor/Tandoor.nix
+++ NixOS/Tandoor/Tandoor.nix
@@ -35,9 +35,9 @@
       AUTH_LDAP_CACHE_TIMEOUT=300;
       AUTH_LDAP_SERVER_URI="ldaps://ldaps.${primaryDomain}:636";
       AUTH_LDAP_BIND_DN="cn=ldapservice,ou=users,DC=ldap,DC=datumine,DC=co.uk";
-      AUTH_LDAP_USER_SEARCH_BASE_DN="DC=ldap,DC=datumine,DC=co.uk";
-      AUTH_LDAP_USER_SEARCH_FILTER_STR="(&(objectClass=user)(memberOf=cn=tandoor,ou=groups,dc=ldap,dc=datumine,dc=co.uk))";
-      AUTH_LDAP_USER_ATTR_MAP="{'first_name': 'fname', 'last_name': 'lname', 'username': 'cn', 'email': 'mail'}";
+      AUTH_LDAP_USER_SEARCH_BASE_DN="ou=users,DC=ldap,DC=datumine,DC=co.uk";
+      AUTH_LDAP_USER_SEARCH_FILTER_STR="(cn=%(user)s)";
+      AUTH_LDAP_USER_ATTR_MAP="{'username': 'cn', 'email': 'mail'}";
     };
   };
 }
