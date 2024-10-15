b50c07c12ae98718f015a5b8673c0841af5bb512
VGHS-lucaruby <lucacrisafi@gmail.com>
2024-10-15 22:32:03 +0100

    Tidy Config and commits

--- NixOS/Teamspeak/Teamspeak.nix
+++ NixOS/Teamspeak/Teamspeak.nix
@@ -1,12 +1,12 @@
-{ lib, ... }:
+{ config, lib, ... }:
 
 {
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
     "teamspeak-server"
-    ];
+  ];
+
   services.teamspeak3 = {
     enable = true;
-
     openFirewall = true;
   };
 }
