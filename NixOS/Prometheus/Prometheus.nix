46b9d53283580e47bccc7e34942d88d12bf618c4
Luca <lucacrisafi@gmail.com>
2024-11-10 04:26:39 +0000

    Create Prometheus Dir

--- NixOS/Prometheus/Prometheus.nix
+++ NixOS/Prometheus/Prometheus.nix
@@ -1,6 +1,8 @@
 { ... }:
 
 {
+  systemd.tmpfiles.rules = [ "d /var/lib/prometheus 0700 prometheus prometheus - -" ];
+
   services.prometheus = {
     enable = true;
     stateDir = "/var/lib/prometheus";
