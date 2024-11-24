42d1ef1916493df383b18b1b79c2661e6b868b32
Luca <lucacrisafi@gmail.com>
2024-11-24 03:56:54 +0000

    initial scraping test.

--- NixOS/Prometheus/Prometheus.nix
+++ NixOS/Prometheus/Prometheus.nix
@@ -3,17 +3,18 @@
 {
   systemd.tmpfiles.rules = [ "d /var/lib/prometheus 0700 prometheus prometheus - -" ];
 
+  # todo secure
   services.prometheus = {
     enable = true;
     stateDir = "prometheus";
-    # alertmanager = {
-    #   enable = true;
-    #   listenAddress = "0.0.0.0";
-    # };
-    # exporters = {
-    #   snmp = {
-    #     enable = true;
-    #   };
-    # };
+    scrapeConfigs = [
+      {
+        # Initial Node For Testing. Will Attempt To Make Config Dynamic In Future.
+        job_name = "DATHOMINECRAFT01";
+        static_configs = [{
+          targets = [ "10.0.20.101:9100" ];
+        }];
+      }
+    ];
   };
 }
