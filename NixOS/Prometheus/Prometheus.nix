b0763340232c828786ed3852f35c9cef03b95847
Luca <lucacrisafi@gmail.com>
2024-12-01 14:44:37 +0000

    Add logging of router. Regex tidy node names.

--- NixOS/Prometheus/Prometheus.nix
+++ NixOS/Prometheus/Prometheus.nix
@@ -8,22 +8,23 @@
     enable = true;
     stateDir = "prometheus";
     scrapeConfigs = [
-      # {
-      #   # Initial Node For Testing. Will Attempt To Make Config Dynamic In Future.
-      #   job_name = "DATHOMINECRAFT01";
-      #   static_configs = [{
-      #     targets = [ "10.0.20.101:9100" ];
-      #   }];
-      # }
+      {
+        job_name = "node-opnsense";
+        static_configs = [{
+          targets = [ "10.0.20.254:9100" ];
+          labels = { instance = "DATHOOPNS01"; };
+        }];
+      }
       {
         job_name = "node";
         dns_sd_configs = [{
-          names = map (node: "${node}.server.arpa") allNodes;
+          names = (map (node: "${node}.server.arpa") allNodes);
           type = "A";
           port = 9100;
         }];
         relabel_configs = [{
           source_labels = [ "__meta_dns_name" ];
+          regex = "([^\.]+)\..+";
           target_label = "instance";
         }];
       }
