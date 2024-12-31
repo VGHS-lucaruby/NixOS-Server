5f024f1d4c9fc3b163197d6ce205a07a0250185c
VGHS-lucaruby <lucacrisafi@gmail.com>
2024-12-31 01:14:35 +0000

    Authentik Metrics

--- NixOS/Prometheus/Prometheus.nix
+++ NixOS/Prometheus/Prometheus.nix
@@ -34,6 +34,19 @@
         }];
       }
       {
+        job_name = "authentik";
+        dns_sd_configs = [{
+          names = [ "DATHOAUTHENTIK01.server.arpa" ];
+          type = "A";
+          port = 9300;
+        }];
+        # relabel_configs = [{
+        #   source_labels = [ "__meta_dns_name" ];
+        #   regex = "([^\.]+)\..+";
+        #   target_label = "instance";
+        # }];
+      }
+      {
         job_name = "hass";
         metrics_path = "/api/prometheus";
         authorization = { credentials_file = config.sops.secrets."Prometheus/hassLongLived".path; };
