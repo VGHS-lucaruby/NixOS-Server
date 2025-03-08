84d3b917000b90190c82b843c8df4a589375c1af
Luca <lucacrisafi@gmail.com>
2025-03-08 23:34:42 +0000

    Test

--- NixOS/VRising/VRising.nix
+++ NixOS/VRising/VRising.nix
@@ -17,6 +17,17 @@ let
 	startServer = pkgs.writeShellScript "StartVRisingServer" '' 
 		WINEPREFIX=/var/lib/SteamDownloader/${steam-app} WINEARCH=win64 ${pkgs.wineWow64Packages.staging}/bin/wine /var/lib/SteamDownloader/${steam-app}/VRisingServer.exe $(echo $(cat ${serverargs})) -password $(cat ${config.sops.secrets."VRising/password".path})
 	'';
+
+	# serverMods = pkgs.fetchzip {
+	# 	url = "https://drive.google.com/uc?export=download&id=1h3gVGW1qHbiYWQNO-iwHscwGLitdXRhT";
+  	# 	hash = "";
+	# };
+
+	serverMods = fetchTarball {
+		url = "https://drive.google.com/uc?export=download&id=1h3gVGW1qHbiYWQNO-iwHscwGLitdXRhT";
+  		sha256 = "sha256:0xs1y3z8z7hj097j6mi8kjdc0nqgbymqdij7a23bac6bd6pd0hna";
+	};
+
 in {
 	
 	sops.secrets = {
@@ -31,6 +42,10 @@ in {
 		# Install the game before launching.
 		wants = [ "SteamDownloader@${steam-app}-windows.service" ];
 		after = [ "SteamDownloader@${steam-app}-windows.service" ];
+
+		preStart = ''	
+			ln -sf ${serverMods}/* ./
+		'';
 
 		serviceConfig = {
 			ExecStart = pkgs.writeShellScript "InitVRisingServer" ''
