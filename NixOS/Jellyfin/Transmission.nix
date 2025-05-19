49ff33e86c08042028bee9adeb6a299a066b3566
Luca <lucacrisafi@gmail.com>
2025-05-19 06:34:44 +0100

    Add WebUi

--- NixOS/Jellyfin/Transmission.nix
+++ NixOS/Jellyfin/Transmission.nix
@@ -4,9 +4,14 @@
   services.transmission = {
     enable = true;
     openFirewall = true;
-    # webHome = pkgs.flood-for-transmission;
+    webHome = pkgs.flood-for-transmission;
+    openRPCPort = true;
     home = "/mnt/media/transmission";
     downloadDirPermissions = "770";
     group = "media"; 
+    package = pkgs.transmission_4;
+    settings = {
+      rpc-bind-address = "0.0.0.0";
+    };
   };
 }
