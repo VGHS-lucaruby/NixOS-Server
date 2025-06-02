915d37cd0b1bb0ab129220938c57d6a42932374b
Luca <lucacrisafi@gmail.com>
2025-06-02 22:41:56 +0100

    Add zsh features

--- NixOS/Base/Zsh.nix
+++ NixOS/Base/Zsh.nix
@@ -3,5 +3,26 @@
 {
   users.defaultUserShell = pkgs.zsh;
   environment.shells = with pkgs; [ zsh ];
-  programs.zsh.enable = true;
+  programs = {
+    zsh = {
+      enable = true;
+      autosuggestions.enable = true;
+      syntaxHighlighting.enable = true;
+      enableLsColors = true;
+      shellInit = "fastfetch";
+      histSize = 1000;
+      shellAliases = {
+        upd = "nixos-rebuild switch -L --refresh --flake github:VGHS-lucaruby/NixOS-Server";
+        updb = "nixos-rebuild boot -L --refresh --flake github:VGHS-lucaruby/NixOS-Server";
+      };
+      oh-my-zsh = {
+        enable = true;
+        theme = "gallois";
+        plugins = [
+          "git"
+          "history"
+        ];
+      };
+    };
+  };
 }
