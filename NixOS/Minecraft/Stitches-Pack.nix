db3715baf90be3292d22219ece75d1567a6a530a
Luca <lucacrisafi@gmail.com>
2025-06-04 22:10:39 +0100

    Java 17 Test

--- NixOS/Minecraft/Stitches-Pack.nix
+++ NixOS/Minecraft/Stitches-Pack.nix
@@ -16,23 +16,26 @@ let
   jvmArgs = ''
     -Xms12G \
     -Xmx12G \
+    -XX:+UseG1GC \
+    -XX:+ParallelRefProcEnabled \
+    -XX:MaxGCPauseMillis=200 \
     -XX:+UnlockExperimentalVMOptions \
-    -XX:+UnlockDiagnosticVMOptions \
-    -XX:+AlwaysActAsServerClassMachine \
-    -XX:+AlwaysPreTouch \
     -XX:+DisableExplicitGC \
-    -XX:+UseNUMA \
-    -XX:AllocatePrefetchStyle=3 \
-    -XX:NmethodSweepActivity=1 \
-    -XX:ReservedCodeCacheSize=400M \
-    -XX:NonNMethodCodeHeapSize=12M \
-    -XX:ProfiledCodeHeapSize=194M \
-    -XX:NonProfiledCodeHeapSize=194M \
-    -XX:-DontCompileHugeMethods \
+    -XX:+AlwaysPreTouch \
+    -XX:G1NewSizePercent=30 \
+    -XX:G1MaxNewSizePercent=40 \
+    -XX:G1HeapRegionSize=8M \
+    -XX:G1ReservePercent=20 \
+    -XX:G1HeapWastePercent=5 \
+    -XX:G1MixedGCCountTarget=4 \
+    -XX:InitiatingHeapOccupancyPercent=15 \
+    -XX:G1MixedGCLiveThresholdPercent=90 \
+    -XX:G1RSetUpdatingPauseTimePercent=5 \
+    -XX:SurvivorRatio=32 \
     -XX:+PerfDisableSharedMem \
-    -XX:+UseFastUnorderedTimeStamps \
-    -XX:+UseCriticalJavaThreadPriority \
-    -XX:+EagerJVMCI
+    -XX:MaxTenuringThreshold=1 \
+    -Dusing.aikars.flags=https://mcflags.emc.gs \
+    -Daikars.new.flags=true
   '';
 
   mcVersion = modpack.manifest.versions.minecraft;
@@ -50,7 +53,7 @@ in {
 
     package = pkgs.forgeServers.${serverVersion}.override { 
       loaderVersion = forgeVersion;
-      jre_headless = pkgs.graalvm-ce;
+      jre_headless = pkgs.jdk17_headless;
     };
     symlinks = {
       "mods" = "${modpack}/mods";
