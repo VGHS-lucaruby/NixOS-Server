05d0fb51c8c9d4bec975daebdf77952f94ecf15e
Luca <lucacrisafi@gmail.com>
2025-06-21 05:16:56 +0100

    Add missed RADIUS Accounting port

--- NixOS/Authentik/Firewall.nix
+++ NixOS/Authentik/Firewall.nix
@@ -5,11 +5,11 @@
     allowedTCPPorts = [ 
       9443 # HTTPS
       6636 # LDAPS
-      1812 # RADIUS
       9300 # Prometheus Metrics
     ];
     allowedUDPPorts = [ 
       1812 # RADIUS
+      1813 # RADIUS Accounting.
     ];
   };
 }
