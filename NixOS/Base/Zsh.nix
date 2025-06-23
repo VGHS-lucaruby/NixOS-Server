729f747e1ef92582739cf2bbbf40bafdbc1ffb1f
VGHS-lucaruby <lucacrisafi@gmail.com>
2025-06-23 04:06:50 +0100

    Update Alias to use Sudo

--- NixOS/Base/Zsh.nix
+++ NixOS/Base/Zsh.nix
@@ -12,8 +12,8 @@
       shellInit = "fastfetch";
       histSize = 1000;
       shellAliases = {
-        upd = "nixos-rebuild switch -L --refresh --flake github:VGHS-lucaruby/NixOS-Server";
-        updb = "nixos-rebuild boot -L --refresh --flake github:VGHS-lucaruby/NixOS-Server";
+        upd = "sudo nixos-rebuild switch -L --refresh --flake github:VGHS-lucaruby/NixOS-Server";
+        updb = "sudo nixos-rebuild boot -L --refresh --flake github:VGHS-lucaruby/NixOS-Server";
       };
       ohMyZsh = {
         enable = true;
