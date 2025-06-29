767cecf0cf34420b799b61bcf3730edfe1554587
VGHS-lucaruby <lucacrisafi@gmail.com>
2025-06-29 20:54:11 +0100

    Re-enable F2B Todo: customise further.

--- NixOS/Base/Fail2Ban.nix
+++ NixOS/Base/Fail2Ban.nix
@@ -1,5 +1,5 @@
 { ... }:
 
 {
-  services.fail2ban.enable = false; # may customize in furture but this is already going to be behind a VPN so slightly overkill.
+  services.fail2ban.enable = true;
 }
