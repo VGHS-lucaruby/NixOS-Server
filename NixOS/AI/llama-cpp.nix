1d730ba67d780be70ba10a1c5cceee6e834fedd2
Luca <lucacrisafi@gmail.com>
2025-09-27 06:20:25 +0100

    Add ngl Arg

--- NixOS/AI/llama-cpp.nix
+++ NixOS/AI/llama-cpp.nix
@@ -14,7 +14,7 @@ in {
     host = "0.0.0.0";
     openFirewall = true;
     model = model;
-    # extraFlags = [ "--jinja "];
+    extraFlags = [ "-ngl" "100"];
   };
 
   systemd.services.llama-cpp = {
