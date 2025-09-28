12aa5d5d32c9abf9ceaac4417e8db44c22ae609b
Luca <lucacrisafi@gmail.com>
2025-09-28 15:45:54 +0100

    Try   Deepseek Tool Calling .

--- NixOS/AI/llama-cpp.nix
+++ NixOS/AI/llama-cpp.nix
@@ -3,10 +3,15 @@
 let
   model = pkgs.fetchurl {
     name = "DeepSeek-R1-Distill-Qwen-32B-Q4_K_M.gguf";
-    url = "https://huggingface.co/bartowski/Meta-Llama-3.1-8B-Instruct-GGUF/resolve/main/Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf?download=true";
-    sha256 = "sha256-ewZPWEK/lTLJFFbe2iiKG2cjl6VPpymqZllShjAzVXw=";
+    url = "https://huggingface.co/bartowski/DeepSeek-R1-Distill-Qwen-7B-GGUF/resolve/main/DeepSeek-R1-Distill-Qwen-7B-Q6_K_L.gguf?download=true";
+    sha256 = "sha256-QCaa0ffmWy7bY1gKkADhYchn4dTP6npTqoCoPEv0n/w=";
   };
 
+  modelTemplate = pkgs.fetchurl {
+    name = "llama-cpp-deepseek-r1.jinja";
+    url = "https://raw.githubusercontent.com/ggml-org/llama.cpp/0124ac989f7e7bf08803788f66dbe4106bdcdd58/models/templates/llama-cpp-deepseek-r1.jinja";
+    sha256 = "sha256-BVSdrSi4WqG+NtnGJb4nN3tBAcEVaGgw95qZSttoeCI=";
+  };
 in {
   services.llama-cpp = {
     enable = true;
@@ -14,7 +19,7 @@ in {
     host = "0.0.0.0";
     openFirewall = true;
     model = model;
-    extraFlags = [ "-ngl" "100" "--jinja" ];
+    extraFlags = [ "-ngl" "100" "--jinja" "--chat-template-file" "${modelTemplate}" ];
   };
 
   systemd.services.llama-cpp = {
