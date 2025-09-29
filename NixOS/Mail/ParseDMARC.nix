76a4e88d1712a10582bb6e2007950c26337f92e4
Luca <lucacrisafi@gmail.com>
2025-09-29 19:22:49 +0100

    Lets try Finally fix dmarc parsing

--- NixOS/Mail/ParseDMARC.nix
+++ NixOS/Mail/ParseDMARC.nix
@@ -3,22 +3,32 @@
 {
   sops.secrets = {
     "Mail/dmarc-reports" = {};
+    "Mail/maxmindLicence" = {};
   };
 
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
     "elasticsearch"
   ];
 
-  services.parsedmarc = {
-    enable = true;
-    settings = {
-      imap = {
-        host = "mail.${primaryDomain}";
-        user = "dmarc-reports@${primaryDomain}";
-        password = "/run/credentials/parsedmarc.service/dmarcPassword";
+  services = {
+    parsedmarc = {
+      enable = true;
+      settings = {
+        imap = {
+          host = "mail.${primaryDomain}";
+          user = "dmarc-reports@${primaryDomain}";
+          password = "/run/credentials/parsedmarc.service/dmarcPassword";
+        };
       };
     };
-    provision.geoIp = false;
+
+    geoipupdate = {
+      settings = {
+        AccountID = 995294;
+        LicenseKey = "/run/credentials/geoipupdate.service/maxmindLicence";
+      };
+    };
   };
   systemd.services.parsedmarc.serviceConfig.LoadCredential = "dmarcPassword:${config.sops.secrets."Mail/dmarc-reports".path}";
+  systemd.services.geoipupdate.serviceConfig.LoadCredential = "maxmindLicence:${config.sops.secrets."Mail/maxmindLicence".path}";
 }
