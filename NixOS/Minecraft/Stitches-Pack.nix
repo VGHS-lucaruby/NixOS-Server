0773ba9349e4d03f110d2a5228706fe3254d1ab6
Luca <lucacrisafi@gmail.com>
2025-11-09 00:11:28 +0000

    Update Pack

--- NixOS/Minecraft/Stitches-Pack.nix
+++ NixOS/Minecraft/Stitches-Pack.nix
@@ -2,8 +2,8 @@
 
 let
   modpack = pkgs.fetchPackwizModpack {
-    url = "https://raw.githubusercontent.com/Boxingflame/Stitches-Pack/1.2.1/pack.toml";
-    packHash = "sha256-kk2O0a0otGhUx//YmmY4abJGanM72VlSOuJUVrYmvrU=";
+    url = "https://raw.githubusercontent.com/Boxingflame/Stitches-Pack/1.2.2/pack.toml";
+    packHash = "sha256-3jGqUsKDpK7CzEXwt7efIBP282zhHkUKMEUHzzs8tQI=";
   };
 
   customPkgs = import ../../CustomPackages { inherit pkgs; };
