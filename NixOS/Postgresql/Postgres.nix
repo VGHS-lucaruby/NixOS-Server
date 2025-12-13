662968e4ea77b33901b8113f356c3529c84dfb41
Luca <lucacrisafi@gmail.com>
2025-12-13 14:30:30 +0000

    Reformat

--- NixOS/Postgresql/Postgres.nix
+++ NixOS/Postgresql/Postgres.nix
@@ -3,6 +3,14 @@
 {
   systemd.tmpfiles.rules = [ "d /var/lib/postgresql/data 0700 postgres postgres - -" ];
 
+  sops.secrets = {
+    "Postgres/admin" = { owner = "postgres"; };
+    "Postgres/authentik" = { owner = "postgres"; };
+    "Postgres/tandoor" = { owner = "postgres"; };
+    "Postgres/grafana" = { owner = "postgres"; };
+    "Postgres/firefly" = { owner = "postgres"; };
+  };
+
   # Use lower case names for DB and users lol
   services = {
     postgresql = {
@@ -49,6 +57,8 @@
           ensureDBOwnership = true;
         }
       ];
+      # Set Passwords
+      # Replace once PR#326306 is upstreamed
       initialScript = pkgs.writeText "init-sql-script" ''
         DO $$
         DECLARE pwdAdmin TEXT;
@@ -79,37 +89,4 @@
       startAt = "18:00";
     };
   };
-
-  # Set Passwords
-  # Replace once PR#326306 is upstreamed
-  sops.secrets = {
-    "Postgres/admin" = { owner = "postgres"; };
-    "Postgres/authentik" = { owner = "postgres"; };
-    "Postgres/tandoor" = { owner = "postgres"; };
-    "Postgres/grafana" = { owner = "postgres"; };
-    "Postgres/firefly" = { owner = "postgres"; };
-  };
-
-  # systemd.services.postgresql.postStart = ''
-  #   $PSQL -tA <<'EOF'
-  #     DO $$
-  #     DECLARE pwdAdmin TEXT;
-  #     DECLARE pwdAuthentik TEXT;
-  #     DECLARE pwdTandoor TEXT;
-  #     DECLARE pwdGrafana TEXT;
-  #     DECLARE pwdFireFly TEXT;
-  #     BEGIN
-  #       pwdAdmin := trim(both from replace(pg_read_file('${config.sops.secrets."Postgres/admin".path}'), E'\n', '''));
-  #       pwdAuthentik := trim(both from replace(pg_read_file('${config.sops.secrets."Postgres/authentik".path}'), E'\n', '''));
-  #       pwdTandoor := trim(both from replace(pg_read_file('${config.sops.secrets."Postgres/tandoor".path}'), E'\n', '''));
-  #       pwdGrafana := trim(both from replace(pg_read_file('${config.sops.secrets."Postgres/grafana".path}'), E'\n', '''));
-  #       pwdFireFly := trim(both from replace(pg_read_file('${config.sops.secrets."Postgres/firefly".path}'), E'\n', '''));
-  #       EXECUTE format('ALTER USER admin PASSWORD '''%s''';', pwdAdmin);
-  #       EXECUTE format('ALTER USER authentik PASSWORD '''%s''';', pwdAuthentik);
-  #       EXECUTE format('ALTER USER tandoor PASSWORD '''%s''';', pwdTandoor);
-  #       EXECUTE format('ALTER USER grafana PASSWORD '''%s''';', pwdGrafana);
-  #       EXECUTE format('ALTER USER firefly PASSWORD '''%s''';', pwdFireFly);
-  #     END $$;
-  #   EOF
-  # '';
 }
